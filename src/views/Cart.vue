ff52ea57a86d44248b186567c744066fbac6f7f9
zhouqiii <860178804@qq.com>
2019-07-10 18:51:31 +0800

    1

---
+++ src/views/Cart.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="cart">
+  <ul>
+      <li v-for='(item,index) in lists' :key='index'>{{item.productName}}</li>
+  </ul>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import dataJson from './cart.json'
+
+export default {
+  name: 'cart',
+  data () {
+      return{
+           lists: []
+      }
+  },
+  created () {
+      this.dataView();
+  },
+  methods:{
+      dataView: function (dataJson) {
+          debugger;
+      this.lists = dataJson.result.list;//就这句话错
+      }
+  }
+  
+}
+</script>
