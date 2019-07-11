90cceb0cc54158d0f1286fe1c947a12294dbda4a
zhouqiii <860178804@qq.com>
2019-07-11 18:53:35 +0800

    4

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -15,13 +15,13 @@
          </tr>
      </table>
      </div>
-     <div style="margin-left:0px;">
-        <ul>
+     <div>
+        <ul style="padding-inline-start:0px;">
             <div style="border:1px solid #A9A9A9;height:100px;">
                 <dl>
                     <dd :style="btn"><input type="checkbox"/></dd>
                     <dd :style="image"><img src="../img/goods-1.jpg" width="70px"/></dd>
-                    <dd :style="proame">香烟</dd>
+                    <div v-for="(item,index) in lists" :key="index" :style="proame">{{item.productName}}</div>
                     <dd :style="give">赠送：</dd>
                     <dd :style="price">￥8</dd>
                     <dd :style="number">
@@ -36,15 +36,19 @@
         </ul>
      </div>
     </div>
-    <div style="text-align:left;margin:10px 20px 0 20px;">
-     <table style="width:100%;vertical-align:center;">
+     <div style="text-align:left;margin:30px 20px 0px 20px;">
+         <input type="checkbox"/>
+         <span style="margin:20px 10px 0 10px;color:#D2691E;">全选</span><span style="margin-right:66%;">取消全选</span>
+         <span>总额：</span>
+         <div :style="money">付款</div>
+     <!--<table style="width:100%;vertical-align:center;">
        <tr>
-        <td><input type="checkbox"/></td>
+       <td><input type="checkbox"/></td>
        <td style="margin-right:10px;">全选</td>
        <td>取消全选</td>
        <td>付款：</td>
        <td style="width:17%;height:10%;background-color:red;">结账</td> 
-       </tr>
+       </tr> -->
     </div>
 </div>
 </template>
@@ -62,54 +66,66 @@ export default {
             height: ''
            },
            project: {
-               height: '',
+               height: '50px',
                backgroundColor: 'DimGray',
                border: '1px solid  #A9A9A9',
+               paddingTop: '15px'
            },
            btn:{
                margin: '0 auto',
                paddingTop: '25px',
                float: 'left',
-               marginLeft: '10px'
+               marginLeft: '20px',
+               width: '1%'
            },
            image: {
                float: 'left',
                marginLeft: '10px',
-               width: '72px'
+               width: '8%'
            },
            proame:{
                float: 'left',
                textAlign: 'left',
                marginLeft: '10px',
-               width: '72px'
+               width: '5%'
            },
            give:{
                float: 'left',
                textAlign: 'left',
                marginLeft: '10px',
-               width: '430px'
+               width: '33%'
            },
            price:{
                float: 'left',
                textAlign: 'left',
                marginLeft: '10px',
-               width: '130px'
+               width: '11%'
            },
             number: {
                 float: 'left',
                 textAlign: 'left',
                 marginLeft: '10px',
-                width: '160px'
+                width: '12%'
            },
            priceAdd: {
                float: 'left',
                textAlign: 'left',
                 marginLeft: '10px',
-                width: '60px',
+                width: '3%',
                 color: 'red'
            },
            del: {
                fontSize: '20px'
+           },
+           money: {
+               float: 'right',
+               height: '40px',
+               width: '15%',
+               backgroundColor: '	#B22222',
+               paddingTop: '10px',
+               textAlign: 'center',
+               color: '#fff',
+               textSize: '20px'
            }
       }
   },
@@ -123,7 +139,7 @@ export default {
       this.lists = data.result.list;
       },
       getBor: function () {
-        this.project.height=window.innerHeight-575+'px';
+        // this.project.height=window.innerHeight-700+'px';
       }
   }
 }
