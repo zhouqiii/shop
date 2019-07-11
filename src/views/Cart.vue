1a87b09227718b51181f8c42c4ef0aa4e0bf1419
zhouqiii <860178804@qq.com>
2019-07-12 01:00:34 +0800

    5

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -17,19 +17,21 @@
      </div>
      <div>
         <ul style="padding-inline-start:0px;">
-            <div style="border:1px solid #A9A9A9;height:100px;">
+            <div v-for="(item,index) in lists" :key="index" style="border:1px solid #A9A9A9;height:100px;">
                 <dl>
-                    <dd :style="btn"><input type="checkbox"/></dd>
-                    <dd :style="image"><img src="../img/goods-1.jpg" width="70px"/></dd>
-                    <div v-for="(item,index) in lists" :key="index" :style="proame">{{item.productName}}</div>
-                    <dd :style="give">赠送：</dd>
-                    <dd :style="price">￥8</dd>
+                    <dd :style="btn" ><input type="checkbox" v-bind:class="{'check':item.checked}" @click="choose(item)"/></dd>
+                    <dd :style="image"><img :src="item.productImage"></dd>
+                    <dd :style="proame">{{item.productName}}</dd>
+                    <dd :style="give" v-for="(part,index) in item.parts" :key="index">赠送：{{part.partsName}}</dd>
+                    <dd :style="price">{{item.productPrice | formatMoney}}</dd>
                     <dd :style="number">
                         <span>
-                            +<input type="text" value="1" style="width:30px;margin:0 5px;"/>-
+                            <span @click="add(item,1)">+</span>
+                            <input type="text" value="0" v-model="item.productQuantity" style="width:30px;margin:0 5px;"/>
+                            <span @click="add(item,-1)">-</span>
                         </span>
                     </dd>
-                    <dd :style="priceAdd">￥11</dd>
+                    <dd :style="priceAdd">{{item.productPrice * item.productQuantity | formatMoney}}</dd>
                     <dd :style="del"><i class="el-icon-delete"></i></dd>
                 </dl>
             </div>
@@ -38,7 +40,7 @@
     </div>
      <div style="text-align:left;margin:30px 20px 0px 20px;">
          <input type="checkbox"/>
-         <span style="margin:20px 10px 0 10px;color:#D2691E;">全选</span><span style="margin-right:66%;">取消全选</span>
+         <span style="margin:20px 10px 0 10px;color:#D2691E;" :class="{'check':checkAllFlag}" @click="checkAll(item)">全选</span><span style="margin-right:66%;">取消全选</span>
          <span>总额：</span>
          <div :style="money">付款</div>
      <!--<table style="width:100%;vertical-align:center;">
@@ -62,6 +64,8 @@ export default {
   data () {
       return{
            lists: [],
+           part: [],
+           checkAllFlag:'false',
            content:{
             height: ''
            },
@@ -81,7 +85,7 @@ export default {
            image: {
                float: 'left',
                marginLeft: '10px',
-               width: '8%'
+               width: '70px'
            },
            proame:{
                float: 'left',
@@ -133,6 +137,11 @@ export default {
       this.dataView();
       this.getBor()
   },
+  filters: {
+    formatMoney: function (value) {
+        return "￥"+value.toFixed(2);
+    } 
+  },
   methods: {
       dataView: function () {
       let data = dataJson;
@@ -140,9 +149,37 @@ export default {
       },
       getBor: function () {
         // this.project.height=window.innerHeight-700+'px';
-      }
+      },
+      add: function (product,way) {
+          if(way>0){
+              product.productQuantity++;
+          }else{
+              product.productQuantity--;
+              if(product.productQuantity<2){
+                  product.productQuantity = 1;
+              }
+          }
+      },
+      choose: function(item) {
+           if(typeof item.checked == 'undefined'){
+               this.$set(item,"checked",true)
+           }else{
+               item.checked = !item.checked;
+           }
+       },
+       checkAll: function(item) {
+           this.checkAllFlag='true';
+           this.lists.forEach(function(item,index) {
+            if(typeof item.checked == 'undefined'){
+               this.$set(item,"checked",true)
+           }else{
+               item.checked = true;
+           }
+           })
+       }
   }
 }
+
 </script>
 <style lang="sass">
 
