8bc2e2db9d2a78557469b1f1b2f21d5e6daf696f
zhouqiii <860178804@qq.com>
2019-07-14 11:34:31 +0800

    9

--- src/views/Adress.vue
+++ src/views/Adress.vue
@@ -14,11 +14,20 @@
   <span style="font-size:18px;font-weight:bold;color:SlateGray;">配送地址</span>
   </el-divider>
 </div>
-<div :style="adressCart" >
-
+<div :style="adressCart1" >
+<div :style="adressContent">
+  <span>JackBean<i class="el-icon-edit" style="margin-left:45%;"></i></span>
+  <p style="font-size:14px;">地址</p>
+  <p style="font-size:14px;margin-top:30px;color:Gray;">12345</p>
+  <p style="color:#D2691E;font-size:14px;">默认地址</p>
 </div>
-<div :style="adressCart" >
-
+</div>
+<div :style="adressCart2" >
+<i class="el-icon-plus"></i>
+<p>添加新地址</p>
+</div>
+<div :style="adressCart3" >
+  <span>more<i class="el-icon-arrow-down"></i></span>
 </div>
 </div>
 </template>
@@ -30,11 +39,31 @@ export default {
     data () {
       return {
       adressLists: [],
-      adressCart: {
+      adressCart1: {
         border: '2px solid LightGrey',
         height: '180px',
         width: '22%',
-        margin: '40px 10px 10px 0px'
+        margin: '35px 10px 10px 0px',
+        position: 'absolute'
+      },
+        adressCart2: {
+        border: '2px solid LightGrey',
+        height: '180px',
+        width: '22%',
+        margin: '35px 10px 10px 25%',
+        position: 'absolute'
+      },
+      adressCart3: {
+      position: 'absolute',
+      marginTop: '18%',
+      color: '#D2691E',
+      marginLeft: '47%'
+      },
+      adressContent: {
+       textAlign: 'left',
+       paddingTop: '20px',
+       paddingLeft: '18px',
+       fontSize: '18px'
       }
       }
     },
@@ -50,7 +79,13 @@ export default {
 }
 </script>
 <style lang="scss">
-
+div .el-icon-edit{
+  font-size: 25px;
+};
+div .el-icon-plus{
+  font-size: 60px;
+  margin-top: 13%;
+}
 </style>
 
 
